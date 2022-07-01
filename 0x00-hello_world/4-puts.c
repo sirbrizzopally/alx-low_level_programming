73c96a69e707aa25f9ed18ae4f6be3dc2a3055fe
Brian Pallangyo <pallangyobrian@yahoo.com>
2022-06-30 19:54:04 -0700

    C programming with excape character example

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main (void)
+/*Programm to print output using puts function and escape character
+ * so that quotes appear in ooutput
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
