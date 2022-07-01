583f9404362e228c9645384fb6ce385241ad6789
Brian Pallangyo <pallangyobrian@yahoo.com>
2022-06-30 20:08:37 -0700

    Using print f function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'using printf function'
+ *
+ * Return: 0 meaning success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
