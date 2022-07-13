55bca82d7a14032ad5d4430c2e6a4a93f9839096
Brian Pallangyo <pallangyobrian@yahoo.com>
2022-07-12 19:58:37 -0700

    Added Key gen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "main.h"
+
+/*
+ * main : function to generate random password
+ *
+ * return: random generated characters
+ */
+
+int main (void)
+{
+	char random_char;
+	int a;
+
+	srand(time(0));
+	while (a<=2645)
+	{
+		random_char = rand() % 128;
+		a = a + random_char;
+		putchar(random_char);
+	}
+	putchar(2772 - a);
+
+	return (0);
+}
